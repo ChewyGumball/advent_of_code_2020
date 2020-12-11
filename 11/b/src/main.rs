0ce690ab28d6b118de8c9c8e47688d9b0a7e7d4a
Ben Dunkin <chewygumball@gmail.com>
2020-12-10 21:38:01 -0800

    question 11

---
+++ 11/b/src/main.rs
@@ -0,0 +1,145 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn parse_line(line: std::io::Result<String>, file_name: String) -> Vec<char> {
+    let value = match line {
+        Err(why) => panic!("Couldn't read line from {}: {}", file_name, why),
+        Ok(line) => line
+    };
+
+    return value.as_str().chars().collect::<Vec<char>>();
+}
+
+fn parse_file(file_name: &Path) -> Vec<Vec<char>> {
+    let file = match File::open(&file_name) {
+        Err(why) => panic!("Couldn't open {}: {}", file_name.display(), why),
+        Ok(file) => file
+    };
+
+    let lines = io::BufReader::new(file).lines();
+
+    return lines.map(|line| parse_line(line, file_name.display().to_string()))
+                .collect();
+}
+
+struct Change {
+    row: usize,
+    column: usize,
+}
+
+fn find_first_visible_seat(map: &Vec<Vec<char>>, row: i32, column: i32, delta_row: i32, delta_column: i32) -> char {
+    let mut current_row = row + delta_row;
+    let mut current_col = column + delta_column;
+
+    let row_range = 0..(map.len() as i32);
+    let col_range = 0..(map[0].len() as i32);
+
+    while row_range.contains(&current_row) && col_range.contains(&current_col) {
+        if map[current_row as usize][current_col as usize] != '.' {
+            return map[current_row as usize][current_col as usize];
+        }
+        current_row += delta_row;
+        current_col += delta_column;
+    }
+
+    return '.';
+}
+
+fn count_occupied_around(map: &Vec<Vec<char>>, row: i32, column: i32) -> usize {
+    let mut occupied = 0;
+
+    for i in -1..2 {
+        for j in -1..2 {
+            if j == 0 && i == 0 {
+                continue;
+            }
+
+            let first_seat = find_first_visible_seat(map, row, column, i, j);
+            if first_seat == '#' {
+                occupied += 1;
+            }
+        }
+    }
+
+    return occupied;
+}
+
+fn find_changes(map: &Vec<Vec<char>>) -> Vec<Change> {
+    let mut changes = Vec::new();
+    
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            let current_value = map[i as usize][j as usize];
+
+            if current_value != '.' {
+                let occupied = count_occupied_around(map, i as i32, j as i32);
+                if (occupied == 0 && current_value == 'L') || (occupied >= 5 && current_value == '#') {
+                    changes.push(
+                        Change {
+                            row: i,
+                            column: j,
+                        }
+                    );
+                }
+            }
+        }
+    }
+
+    return changes;
+}
+
+fn apply_changes(map: &mut Vec<Vec<char>>, changes: &Vec<Change>) {
+    for change in changes {
+        if map[change.row][change.column] == 'L' {
+            map[change.row][change.column] = '#';
+        } else {
+            map[change.row][change.column] = 'L';
+        }
+    }
+}
+
+fn count_occupied(map: &Vec<Vec<char>>) -> usize {
+    let mut occupied = 0;
+
+    for row in map {
+        for seat in row {
+            if *seat == '#' {
+                occupied += 1;
+            }
+        }
+    }
+
+    return occupied;
+}
+
+fn print_map(map: &Vec<Vec<char>>) {
+    for row in map {
+        for seat in row {
+            print!("{}", seat);
+        }
+        print!("\n");
+    }
+    print!("\n");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input_file = Path::new(&args[1]);
+
+    let mut map = parse_file(&input_file);
+    
+    let mut should_procced = true;
+    while should_procced {
+        //print_map(&map);
+        let changes = find_changes(&map);
+        if changes.is_empty() {
+            should_procced = false;
+            let occupied_seats = count_occupied(&map);
+            print!("Occupied seats: {}", occupied_seats);
+        } else {
+            apply_changes(&mut map, &changes);
+        }
+    }
+}
